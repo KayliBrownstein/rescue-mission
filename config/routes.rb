8e9f3b382cabf0bc8f5c6d19d2be8d18cfea18e7
KayBrownstein <kaylivbrownstein@gmail.com>
2017-04-13 21:10:20 -0400

    added github user signin with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :questions do
     resources :answers, only: [:new, :create]
   end
+  
+  root 'questions#index'
 end
